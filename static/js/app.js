f9fdf68c30918bd135dae5eef1f7fedab7e9193c
Fraol Abebe <fraolabebe@Fraols-Air.fios-router.home>
2021-01-22 17:04:16 -0500

    pushing Js scripts

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,19 @@
 showData(data);
-d3.select('button').on('click', handleClick);
-
 function showData(data) {
     d3.select('tbody').html('');
     data.forEach(obj => {
         var row = d3.select('tbody').append('tr');
-        Object.values(obj).forEach(val =>{
-            var cell = row.append('td');
+        Object.values(obj).forEach(val => {
+            var cell = row.append('td')
             cell.text(val);
         });
     });
 };
 
-function handleClick() {
+d3.select('button').on('click', () => {
     var date = d3.select('input').property('value');
-    var fitleredData = data.filter(obj => obj.datetime == date);
-    showData(fitleredData);
-};
+    var filteredData = data.filter(obj => obj.datetime == date);
+    showData(filteredData);
+})
 
 
-
-
-// indentify the table and tbody
-
-d3.select('#ufo-table>tbody')
-
-
-//dynamically build the table
-
-
-tableData.forEach(obj => {
-    var row = tbody
-    
-});
